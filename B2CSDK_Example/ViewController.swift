81b743257777170e0038d3fde5bc2a94817880d2
Raj Kadam <rajendra.kadam@degpeg.com>
2022-06-22 18:55:45 +0530

    B2C SDK Integration

--- B2CSDK_Example/ViewController.swift
+++ B2CSDK_Example/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import B2CSDK
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func actionDegpegSDKDemo(_ sender: UIButton) {
+        print(#function)
+        openSDK()
+    }
 
+    func openSDK(){
+        // 1. app id
+        // 2. content publisher Id / content prover id
+        // 3. app secret
+        let appId = "degpegdegpeg _mediaXuUwyvni"
+        let secretKey = "Nnra8P2iGqT2uJFU"
+        let manager = DegpegManager.init(appID: appId, secret: secretKey, userId: "6278c4556cb38a7a9c10df6e", userName: "Raj Kadam", influencerID: "6278c4546cb38a7a9c10df6d")
+        if let vc = manager.getRootViewController() {
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
 }
 
